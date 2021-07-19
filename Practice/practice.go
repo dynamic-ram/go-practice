f6af4c3822227f96b3a11fd034a1f5cf3ada9bb6
dynamic-ram <ramsbharath@yahoo.com>
2021-07-19 19:16:35 +0530

    for, if and switch

---
+++ Practice/practice.go
@@ -0,0 +1,11 @@
+package Practice
+
+import (
+	"fmt"
+)
+
+func forLoop() {
+	for i := 1; i <= 10; i++ {
+		fmt.Println(i)
+	}
+}
--- main.go
+++ main.go
@@ -1,9 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter a Value less than 10: ")
+	input, _ := reader.ReadString('\n')
+	value, _ := strconv.ParseInt(strings.TrimSpace(input), 10, 64)
+	for i := value; i <= 10; i++ {
+		if i%2 == 0 {
+			fmt.Println(i, "is Even")
+		} else {
+			fmt.Println(i, "is Odd")
+		}
+		switch i {
+		case 1:
+			fmt.Println(i, "is January")
+		case 7:
+			fmt.Println(i, "is July")
+		case 8:
+			fmt.Println(i, "is August")
+		case 9:
+			fmt.Println(i, "is September")
+		case 10:
+			fmt.Println(i, "is October")
+		default:
+			fmt.Printf("%v is not in the list\n", i)
+		}
+	}
+	fmt.Println("\nHello World")
 }
