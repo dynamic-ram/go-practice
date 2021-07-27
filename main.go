bbb008f0c67cda7390973c242e0e002fdd221e12
dynamic-ram <ramsbharath@yahoo.com>
2021-07-27 15:52:23 +0530

    Updated

--- main.go
+++ main.go
@@ -1,32 +1,41 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
-func CheckPrime(number int) {
+func CheckPrime(number int) bool {
 	isPrime := true
-	if number == 0 || number == 1 {
-		fmt.Printf(" %d is not a prime number\n", number)
+	if number <= 3 || number > 100 {
+		isPrime = false
 	} else {
 		for i := 2; i <= number/2; i++ {
 			if number%i == 0 {
-				fmt.Printf(" %d is not a prime number\n", number)
 				isPrime = false
 				break
 			}
 		}
 		if isPrime {
-			fmt.Printf(" %d is a prime number\n", number)
 			if number%4 == 1 {
-				fmt.Printf(" %d is a pythagorean prime number\n", number)
+				isPrime = true
+			} else {
+				isPrime = false
 			}
 		}
 	}
+	return isPrime
 }
 func main() {
-	var number int
-	fmt.Print("Enter Number To check Prime:")
-	fmt.Scanln(&number)
-	CheckPrime(number)
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	number, _ := strconv.ParseInt(strings.TrimSpace(input), 10, 64)
+	if CheckPrime(int(number)) {
+		fmt.Println("TRUE")
+	} else {
+		fmt.Println("FALSE")
+	}
 }
