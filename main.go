e38bf1b7fc667da81b1037600fe54726084439ba
dynamic-ram <ramsbharath@yahoo.com>
2021-07-27 17:08:16 +0530

    arrays,slices and maps

--- main.go
+++ main.go
@@ -1,38 +1,43 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strconv"
-	"strings"
-)
+import "fmt"
 
 func main() {
+	/*Reading Input and converting String to Int
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Enter a Value less than 10: ")
+	fmt.Println("Enter a Value: ")
 	input, _ := reader.ReadString('\n')
 	value, _ := strconv.ParseInt(strings.TrimSpace(input), 10, 64)
-	for i := value; i <= 10; i++ {
-		if i%2 == 0 {
-			fmt.Println(i, "is Even")
-		} else {
-			fmt.Println(i, "is Odd")
-		}
-		switch i {
-		case 1:
-			fmt.Println(i, "is January")
-		case 7:
-			fmt.Println(i, "is July")
-		case 8:
-			fmt.Println(i, "is August")
-		case 9:
-			fmt.Println(i, "is September")
-		case 10:
-			fmt.Println(i, "is October")
-		default:
-			fmt.Printf("%v is not in the list\n", i)
-		}
-	}
-	fmt.Println("\nHello World")
+	*/
+	/* Arrays and Slices
+	var x = make([]int, 5, 10)
+	x = append(x, 3, 4)
+	var y = [5]int{1, 2, 3, 4, 5}
+	var z = x[0:3]
+	var q = copy(x, z)
+	fmt.Println(value)
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+	fmt.Println(q)
+	var x = make([]int, 3) //[0,0,0]
+	var y = []int{1, 2, 3} //[1,2,3]
+	fmt.Println(x)
+	copy(x, y) //[1,2,3]
+	fmt.Println(y)
+	fmt.Println(x)
+	*/
+	x := [6]int{1, 2, 3, 4, 5, 6}
+	z := x[3:5]
+	fmt.Println(z)
+
+	//Maps
+	/*
+		var x = make(map[string]int)
+		x["key0"] = 1
+		x["key1"] = 9
+		fmt.Println(x["key0"])
+		delete(x, "key0")
+		fmt.Println(x["key0"])
+	*/
 }
