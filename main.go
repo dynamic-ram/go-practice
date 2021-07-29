e1dcbba865912fc9af628c3b41ef85df61fd95bf
dynamic-ram <ramsbharath@yahoo.com>
2021-07-29 17:55:58 +0530

    Updated with fresco handson

--- main.go
+++ main.go
@@ -1,14 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	/*Reading Input and converting String to Int
+	//Reading Input and converting String to Int
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Enter a Value: ")
 	input, _ := reader.ReadString('\n')
-	value, _ := strconv.ParseInt(strings.TrimSpace(input), 10, 64)
-	*/
+	len, _ := strconv.ParseInt(strings.TrimSpace(input), 10, 64)
+	l := int(len)
+	var x = make([]int, l)
+	var z int
+	var k int
+	for i := 0; i < l; i++ {
+		input, _ := reader.ReadString('\n')
+		y, _ := strconv.ParseInt(strings.TrimSpace(input), 10, 64)
+		x[i] = int(y)
+		if i == 0 || i%2 == 0 {
+			z += x[i]
+		} else {
+			k += x[i]
+		}
+	}
+	fmt.Println(x)
+	fmt.Println(z)
+	fmt.Println(k)
+	fmt.Println(z - k)
 	/* Arrays and Slices
 	var x = make([]int, 5, 10)
 	x = append(x, 3, 4)
@@ -26,11 +49,10 @@ func main() {
 	copy(x, y) //[1,2,3]
 	fmt.Println(y)
 	fmt.Println(x)
-	*/
 	x := [6]int{1, 2, 3, 4, 5, 6}
 	z := x[3:5]
 	fmt.Println(z)
-
+	*/
 	//Maps
 	/*
 		var x = make(map[string]int)
