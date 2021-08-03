a448ec6d9eaa3cf9c5714e5f703aec9ee2444a35
dynamic-ram <ramsbharath@yahoo.com>
2021-08-03 12:49:53 +0530

    Narcissistic numbers

--- main.go
+++ main.go
@@ -1,22 +1,55 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
+/*
 func operation1(num1 int, num2 int) int {
 	return num1 + num2
 }
 func operation2(num1 int, num2 int) (int, int) {
 	return num1 + num2, num1 - num2
 }
+*/
+
+func isNarcissistic(number int) bool {
+	tmp := number
+	digits := len(strconv.Itoa(number))
+	sum := 0
+	div := 0
+	for {
+		if tmp <= 0 {
+			break
+		}
+		div = tmp % 10
+		temp := 1
+		for i := 0; i < digits; i++ {
+			temp = temp * div
+		}
+		sum += temp
+		tmp = tmp / 10
+	}
+	return number == sum
+}
+
 func main() {
-	/*Reading Input and converting String to Int
+	//Reading Input and converting String to Int
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Enter a Value: ")
 	input, _ := reader.ReadString('\n')
-	len, _ := strconv.ParseInt(strings.TrimSpace(input), 10, 64)*/
-	var a int = 100
+	i, _ := strconv.ParseInt(strings.TrimSpace(input), 10, 64)
+	if isNarcissistic(int(i)) {
+		fmt.Println("True")
+	} else {
+		fmt.Println("False")
+	}
+
+	/*var a int = 100
 	var b int = 80
 	var num = operation1(a, b) // calling function add()
 	fmt.Println("SUM:", num)
@@ -26,4 +59,6 @@ func main() {
 	var add, sub = operation2(c, d)
 	fmt.Println("SUM:", add)
 	fmt.Println("SUB:", sub)
+	*/
+
 }
